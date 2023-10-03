c7c456ab08740a5334d0e5e6c0ba6c6e909b6dbb
JosueHernand <joseh1337@gmail.com>
2023-10-03 16:34:47 -0700

    Made questions in the index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'No License'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can users contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can users run tests for your project?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
 function init() {}
 
-// Function call to initialize app
 init();
