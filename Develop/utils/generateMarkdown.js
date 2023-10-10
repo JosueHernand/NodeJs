d0249bc3cd4beb8684cd04c305400af90fc86756
JosueHernand <joseh1337@gmail.com>
2023-10-09 18:33:38 -0700

    Created functions to return licenses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,41 @@
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPLv3') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else {
+    return '';
+  }
+}
 
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'Apache 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+  } else if (license === 'GPLv3') {
+    return 'https://www.gnu.org/licenses/gpl-3.0';
+  } else {
+    return '';
+  }
+}
 
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
+  if (badge && link) {
+    return `
+## License
+
+This project is licensed under the [${license}](${link}) license. ${badge}
+`;
+  } else {
+    return '';
+  }
+}
 
 function generateMarkdown(data) {
   return `
